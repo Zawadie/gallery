8628406f69f1bc5fc33eaab7be15bd15ea5d0b9f
Zawadie <Zogbonna.zo@gmail.com>
2023-02-26 20:18:13 +0300

    tests

--- server.js
+++ server.js
@@ -2,33 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// Initializing the app
-const app = express();
-
 // connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
+let mongodb_url = 'mongodb+srv://zawadi:I1d6pkxxDfGnq4EY@cluster0.beyzcra.mongodb.net/?retryWrites=true&w=majority';
+let dbName = 'cluster0';
+mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
+    if (err) console.log(err)
 });
 
 // test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
+let db = mongoose.connection;
+db.once('open', ()=>{
+    console.log('Database connected successfully')
+})
 
-
+// Initializing the app
+const app = express();
 
 
 // View Engine
@@ -51,6 +44,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
-
-
-module.exports = app;
